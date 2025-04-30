1f3066496fba5eec053895776c24312a3b3ea7ee
Makhabat Arykova <mhbtarykova@gmail.com>
2025-04-30 12:05:31 +0600

    Family.jsx

---
+++ src/mart/Family.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export const Family = () => {
+  return (
+    <div>
+        <h1>Hello JS-17</h1>
+        <h1>Hello JS-17</h1>
+        <h1>Hello JS-17</h1>
+        <h1>Hello JS-17</h1>
+        <h1>Hello JS-17</h1>
+    </div>
+  )
+}
